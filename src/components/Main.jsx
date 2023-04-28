6d19ad10159902d07d936c3e55a06f0e664127bd
John Chacpi <johnchacpi479@gmail.com>
2023-04-27 21:49:44 -0500

    first section proto

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import { Container } from '@mui/material';
+import React from "react";
+import { Container, Typography } from "@mui/material";
 
 const Main = () => {
   return (
-    <div className='w-full'></div>
-  )
-}
+    <div className="w-full h-[70vh] relative">
+      <div className="w-full h-full titleBackground -z-10 absolute bg-fixed"></div>
+      <Container
+        className="h-full backdrop-blur-sm"
+        sx={{ display: "flex", flexDirection: 'column', justifyContent: "center", alignItems: "center" }}
+      >
+        <Typography
+          variant="h1"
+          noWrap
+          sx={{
+            mr: 3,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".4rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          BOOKFINDER
+        </Typography>
+        <p>Discover your next favorite read with ease</p>
+      </Container>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Main from '@/components/Main'
+import Main from '@/components/Main';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <>
       <Main />
+      <div className='h-[100vh] z-[1]'></div>
     </>
   )
 }
