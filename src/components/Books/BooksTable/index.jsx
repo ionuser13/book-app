559ae01d29dda182127ceaebc807813d2180ff5c
John Chacpi <johnchacpi479@gmail.com>
2023-04-29 15:55:46 -0500

    final details for functional version

--- src/components/Books/BooksTable/index.jsx
+++ src/components/Books/BooksTable/index.jsx
@@ -60,7 +60,7 @@ export default function BooksTable() {
   
   return (
     <div className="w-full">
-      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }} id="home">
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
--- src/pages/works/[id]/index.jsx
+++ src/pages/works/[id]/index.jsx
@@ -3,23 +3,23 @@ import { useRouter } from 'next/router';
 import { Container } from '@mui/material';
 import Footer from '@/components/Footer';
 import Link from 'next/link';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 const URL = 'https://openlibrary.org';
 
 
-const index = () => {
+const Index = () => {
     const router = useRouter();
     const [bookData, setBookData] = useState({});
     const [loading, setLoading] = useState(true);    
+    const {id} = router.query;
 
     useEffect(()=> {
-        const {id} = router.query;
         if (!router.isReady) return
         async function getBook(){
             try {
                 const response = await fetch(`${URL}/works/${id}.json`)
                 const dataAnswer = await response.json();
-                console.log(dataAnswer)
                 setBookData(dataAnswer)
                 setLoading(false)
             }
@@ -28,7 +28,7 @@ const index = () => {
             }
         }
         getBook();
-    }, [router?.isReady]);
+    }, [id, router?.isReady]);
 
     if(loading) {
         return (
@@ -40,6 +40,9 @@ const index = () => {
     <div className='w-full h-screen relative mt-8 sm:h-[70vh]'>
         <div className="w-full h-[30%] titleBackground -z-10 relative bg-fixed"></div>
         <Container sx={{marginTop: 2, overflowWrap: 'break-word'}}>
+            <Link href={'/'} className='block mb-2'>
+                <ArrowBackIosIcon />
+            </Link>
             <h3 className='text-3xl font-bold'>{bookData.title}</h3>
 
             <div className='mt-2'>
@@ -62,4 +65,4 @@ const index = () => {
   )
 }
 
-export default index
+export default Index
