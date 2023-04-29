7cc41a4fc5e7875ac745faba7d546cf78964d165
John Chacpi <johnchacpi479@gmail.com>
2023-04-29 15:12:08 -0500

    added information pages, changed routes

--- src/containers/BooksSection/index.jsx
+++ src/containers/BooksSection/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import SearchBar from '@/components/Books/SearchBar';
 import { Container } from '@mui/material';
-import top100Films from "@/public/data.json";
+
 import BooksTable from '@/components/Books/BooksTable';
 
 const Books = () => {
   return (
     <div>
       <Container sx={{display: 'flex', flexDirection: 'column', gap: 8, marginY: 8}}>
-        <SearchBar list={top100Films}></SearchBar>
+        <SearchBar></SearchBar>
         <BooksTable />
       </Container>
     </div>
