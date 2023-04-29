7312d202156a1c200d0aba59842f21b55e465139
John Chacpi <johnchacpi479@gmail.com>
2023-04-28 23:01:39 -0500

    fixed api call

--- src/components/Books/SearchBar/index.jsx
+++ src/components/Books/SearchBar/index.jsx
@@ -3,48 +3,42 @@ import TextField from "@mui/material/TextField";
 import { Container } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import { useGlobalContext } from "src/context/context";
+import { useRouter } from "next/router";
 
-// import Paper from '@mui/material/Paper';
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
-// import TableHead from '@mui/material/TableHead';
-// import TablePagination from '@mui/material/TablePagination';
-// import TableRow from '@mui/material/TableRow';  
+export default function SearchBar({ list }) {
+  const [searchedTerm, setSearchedTerm] = React.useState("");
+  const { setSearchTerm, setResultHeader } = useGlobalContext();
 
-export default function SearchBar({list}) {
+  const handleSearch = async () => {
+    if (searchedTerm.length === 0) {
+      setSearchTerm("The Lord of the Rings");
+      setResultHeader("Please enter a book title...");
+    } else {
+      setSearchTerm(searchedTerm);
+    }
+  };
+
   return (
     <Container>
-      <div className="formcontainer w-full">
-        <form action="/" className="searchForm">
-          <div className="flex align-middle bg-white py-[1.4rem] px-[2.8rem] rounded-3xl text-3xl p-[0.6rem] w-full">
-            <input type="text" name="book name" id="bookName" placeholder="Harry Potter and..." className="text-xl p-[0.6rem] w-full" />
-            <button type="submit" className="flex flex-col">
-              <SearchIcon />
-            </button>
-          </div>
-        </form>
+      <div className="formcontainer w-full mb-2">
+        <div className="flex w-full gap-4">
+          <TextField
+            label="Search by name"
+            sx={{width: '100%'}}
+            onChange={(e) => setSearchedTerm(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="h-fit my-auto"
+            onClick={handleSearch}
+          >
+            <SearchIcon />
+          </button>
+        </div>
       </div>
-      <Stack spacing={2} sx={{ width: 'full' }}>
-        <Autocomplete
-          freeSolo
-          id="free-solo-2-demo"
-          disableClearable
-          options={list.map((option) => option.title)}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Search input"
-              InputProps={{
-                ...params.InputProps,
-                type: "search",
-              }}
-            />
-          )}
-        />
-      </Stack>
+      <Stack spacing={2} sx={{ width: "full" }}></Stack>
     </Container>
   );
 }
--- src/context/context.js
+++ src/context/context.js
@@ -2,10 +2,10 @@ import React, {useState, useContext, useEffect} from 'react';
 import { useCallback } from 'react';
 
 const AppContext = React.createContext();
-const API = 'https://openlibrary.org/search.json?title';
+const API = 'https://openlibrary.org/search.json?title=';
 
 const AppProvider = ({children}) => {
-    const [searchTerm, setSearchTerm] = useState('the lord of the rings');
+    const [searchTerm, setSearchTerm] = useState('The Lord of the Rings');
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(true);
     const [resultHeader, setResultHeader] = useState("");
@@ -13,20 +13,20 @@ const AppProvider = ({children}) => {
     const fetchBooks = useCallback(async() => {
         setLoading(true);
         try {
-            const response = await fetch(`${API}=${searchTerm}`);
+            const response = await fetch(`${API}${searchTerm}`);
             const data = await response.json();
             const {docs} = data;
-            console.log(docs);
+            console.log(docs)
             if(docs) {
-                const newBooks = docs.slice(0,20).map(
+                const newBooks = docs.slice(0, 20).map(
                     (book) => {
                         const {key, author_name, first_publish_year, subject} =  book;
                         return {
                             id: key || 'id not found',
                             author: author_name || 'author not found',
                             PublicationDate: first_publish_year || 'Date not found',
-                            genre: subject[0] || 'genre not found',
-                            description: subject[5] || 'description not available',
+                            genre: subject[0] ||'genre not found',
+                            description: subject[0] || 'description not available',
                         }
                     }
                 );
